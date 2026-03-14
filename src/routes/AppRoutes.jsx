737927bb9e897a3775e254fba18c92fa146796d6
Babusha Kolhe <kkbabu980@gmail.com>
2026-03-15 00:12:35 +0530

    aaa

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import About from "../pages/About";
+// import About from "../pages/About";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
+      {/* <Route path="/about" element={<About />} /> */}
     </Routes>
   );
 };
